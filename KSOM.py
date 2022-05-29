01fa88b1f1598831212c45089b1e93dfbe99d2b4
Yuval sandler <sandler2806@gmai.com>
2022-05-29 15:42:55 +0300

    add update

--- KSOM.py
+++ KSOM.py
@@ -5,12 +5,13 @@ import numpy as np
 
 
 class KSOM:
-    def __init__(self, num_of_clusters=(2, 100), alpha=0.6, r=0):
+    def _init_(self, h, w, num_of_clusters=100, alpha=0.6, r=0):
         self.learning_rate = alpha
         self.num_of_clusters = num_of_clusters
         # maybe change to tuple once we do part 1.2
         # uniform distribution for the weights
-        self.clusters = np.random.rand(num_of_clusters)
+        self.clusters = np.random.rand((h, w))
+        self.shape = (h, w)
         self.radius = r
 
     def fit(self, input_data, num_of_iterations):
@@ -29,18 +30,36 @@ class KSOM:
                     if best_Dj > D_j:
                         best_Dj = D_j
                         best_j = j
-                self.update_weights(best_j, point, t)
+                self.update_weights_1D(best_j, point)
             self.update_learning_rate(t)
             self.update_radius(t)
-            
-    def update_weights(self, best_j, current_point, t):
-        print("yuvi needs to write this code")
+
+    def update_weights_1D(self, best_j, current_point):
+        best_n = np.array([1, best_j])
+        for i in range(self.num_of_clusters):
+            dist = np.linalg.norm(best_n - np.array([1, i]))
+            radius = np.exp(-dist ** 2 / (2 * self.radius ** 2))  # check that minus out of **
+            self.clusters[i][0] += self.learning_rate * radius * (current_point[0] - self.clusters[best_j][0])
+            self.clusters[i][1] += self.learning_rate * radius * (current_point[1] - self.clusters[best_j][1])
+
+        pass
+
+    def update_weights_2D(self, best_j: tuple, current_point: tuple):
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                dist = np.linalg.norm(best_j - np.array([i, j]))
+                radius = np.exp(-dist ** 2 / (2 * self.radius ** 2))  # check that minus out of **
+
+                self.clusters[i, j][0] += self.learning_rate * radius * (
+                            current_point[0] - self.clusters[best_j[0], best_j[1]][0])
+                self.clusters[i, j][1] += self.learning_rate * radius * (
+                            current_point[1] - self.clusters[best_j[0], best_j[1]][1])
 
     def update_learning_rate(self, t):
         # lessen the learning rate
-        self.learning_rate = 0.9*(1-(t/1000))
+        self.learning_rate = 0.9 * (1 - (t / 1000))
         pass
 
     def update_radius(self, t):
-        # update the radius
+        self.radius *= 0.9 * (1 - (t / 1000))
         pass
