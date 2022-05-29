53a02e353c44fa78025d16c86c3a1f2f7709b0d4
ShauliTaragin <shaulitaragin@gmail.com>
2022-05-29 14:34:49 +0300

    done checking best_dj + added files

--- KSOM.py
+++ KSOM.py
@@ -1,9 +1,11 @@
+import sys
+
 import numpy
 import numpy as np
 
 
 class KSOM:
-    def __init__(self, num_of_clusters = (2,100), alpha = 0.6 , r = 0):
+    def __init__(self, num_of_clusters=(2, 100), alpha=0.6, r=0):
         self.learning_rate = alpha
         self.num_of_clusters = num_of_clusters
         # maybe change to tuple once we do part 1.2
@@ -11,8 +13,22 @@ class KSOM:
         self.clusters = np.random.rand(num_of_clusters)
         self.radius = r
 
-
-    def fit(self , input_data , num_of_iterations):
+    def fit(self, input_data, num_of_iterations):
         for t in num_of_iterations:
+            # check if we need to make the points random
+            for point in input_data:
+                # parameters to hold info from the following loop
+                best_Dj = sys.maxsize
+                best_j = 0
+                # run on all clusters
+                for j in range(self.num_of_clusters[1]):
+                    # dj for x   =  w_ij   -  x
+                    d_j_x = (self.clusters[0][j] - point[0])
+                    d_j_y = (self.clusters[1][j] - point[1])
+                    D_j = d_j_x ** 2 + d_j_y ** 2
+                    if best_Dj > D_j:
+                        best_Dj = D_j
+                        best_j = j
+                self.update_weights(best_j, point,t)
 
-
+    def update_weights(self , best_j,current_point,t):
---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import json
+
+
+def createDataSet():
+    points = []
+    while len(points) != 1000:
+        x = np.random.randint(0, 1000)
+        y = np.random.randint(0, 1000)
+        if (x / 1000, y / 1000) not in points:
+            points.append((x / 1000, y / 1000))
+    return points
+
+
+def saveDataSet(DataSetPoints):
+    data = {}
+    for i in range(len(DataSetPoints)):
+        data[str(i)] = {"x": DataSetPoints[i][0], "y": DataSetPoints[i][1]}
+    with open('dataset1.json', 'w') as f:
+        json.dump(data, f)
+        print("saved")
+
+
+if __name__ == '__main__':
+    points = createDataSet()
+    saveDataSet(points)
