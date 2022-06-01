1cb318194be30627aec31316a944b6d8445726c1
LiorBreitman8234 <liorbreitman321@gmail.com>
2022-06-01 14:10:33 +0300

    update stuff

--- KSOM.py
+++ KSOM.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class KSOM:
-    def __init__(self, h, w, num_of_clusters=100, alpha=0.6, r=1):
+    def __init__(self, h, w, num_of_clusters=100, alpha=0.35, r=15):
         self.learning_rate = alpha
         self.num_of_clusters = num_of_clusters
         # maybe change to tuple once we do part 1.2
@@ -12,7 +13,7 @@ class KSOM:
             temp_clusters = np.random.rand(w, 2)
             self.clusters = [[[i[0], i[1]] for i in temp_clusters]]
         else:
-            self.clusters = [[[0,0]] * 10 for i in range(10)]
+            self.clusters = [[[0, 0]] * 10 for i in range(10)]
             for i in range(10):
                 for j in range(10):
                     # since we have a 10x10 cluster matrix we want each point in the matrix to be in order
@@ -20,11 +21,30 @@ class KSOM:
                     x = np.random.uniform(i * 0.1, i * 0.1 + 0.1)
                     y = np.random.uniform(j * 0.1, j * 0.1 + 0.1)
                     self.clusters[i][j] = [x, y]
+        x_model_a = []
+        y_model_a = []
         self.shape = (h, w)
         self.radius = r
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                x_model_a.append(self.clusters[i][j][0])
+                y_model_a.append(self.clusters[i][j][1])
+        plt.scatter(x_model_a, y_model_a, color="red")
+        plt.show()
+
+    def plotClusters(self):
+        x_model_a = []
+        y_model_a = []
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                x_model_a.append(self.clusters[i][j][0])
+                y_model_a.append(self.clusters[i][j][1])
+        plt.scatter(x_model_a, y_model_a, color="red")
+        plt.show()
 
     def fit(self, input_data, num_of_iterations):
         for t in range(num_of_iterations):
+            print(f"iter number :{t}")
             # check if we need to make the points random
             for point in input_data:
                 # parameters to hold info from the following loop
@@ -41,7 +61,9 @@ class KSOM:
                             best_j = [i, j]
                 self.update_weights(best_j, point)
             self.update_learning_rate(t)
+            print(self.learning_rate)
             self.update_radius(t)
+            # print(self.clusters)
 
     def update_weights_1D(self, best_j, current_point):
         best_n = np.array([1, best_j])
@@ -56,16 +78,19 @@ class KSOM:
     def update_weights(self, best_j: list, current_point: tuple):
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                dist = np.linalg.norm(best_j - np.array([i, j]))
+                dist = np.linalg.norm(np.asarray(best_j) - np.array([i, j]))
                 radius = np.exp(-dist ** 2 / (2 * self.radius ** 2))  # check that minus out of **2
+                # print(self.clusters[i][j])
                 self.clusters[i][j][0] += self.learning_rate * radius * (
                         current_point[0] - self.clusters[best_j[0]][best_j[1]][0])
                 self.clusters[i][j][1] += self.learning_rate * radius * (
                         current_point[1] - self.clusters[best_j[0]][best_j[1]][1])
+                # print(self.clusters[i][j])
+                # print()
 
     def update_learning_rate(self, t):
         # lessen the learning rate
-        self.learning_rate = 0.9 * (1 - (t / 1000))
+        self.learning_rate *= 0.9 * (1 - (t / 1000))
         pass
 
     def update_radius(self, t):
